0f5fe770d1c4234cbf22b21d2322cc4b924eff26
igor <gob1in@Mail.ru>
2022-03-03 18:37:41 +0300

    People

--- src/my/people/Man.java
+++ src/my/people/Man.java
@@ -1,6 +1,16 @@
 package my.people;
 
 public class Man {
+    public Man(){
+        age = 18;
+    }
+    public Man(int age) {
+        this.age = age;
+    }
 
     int age = 18;
+    public int getAge(){
+
+        return age;
+    }
 }
