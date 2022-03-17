024a1971e429992c37117e630f03e319c56041db
igor <gob1in@Mail.ru>
2022-03-17 21:10:32 +0300

    Oop

---
+++ src/my/oop/Bicycle.java
@@ -0,0 +1,11 @@
+package my.oop;
+
+public class Bicycle implements Movable{
+    @Override
+    public void move() {
+        System.out.println("Bicycle is moving...");
+    }
+    public void stop() {
+        System.out.println("Bicycle stopped");
+    }
+}
---
+++ src/my/oop/Movable.java
@@ -0,0 +1,5 @@
+package my.oop;
+
+public interface Movable {
+    void move();
+}
